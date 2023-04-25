f984e6a7beac865166defc600fdf79fe8eefb634
Rulis23 <raul.valo14@gmail.com>
2023-04-24 20:10:09 -0600

    ADD: Variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+name="Raul"
+age=27
+last_name="Vasquez"
+list_shopping=["milk","eggs","vegetables"]
+
+print (f"{name}{age}")
+print (name+" "+age) #Esto es lo mismo que comando anterior
+print(list_shopping)
